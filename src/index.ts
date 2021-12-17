bcdb16c5cd34ec6fe0d571ac411920aad4b9924f
Pablo <pablorsantos15@gmail.com>
2021-12-17 16:36:20 -0300

    chore: eslint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import express from 'express'
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
-const app = express()
+const app = express();
 
-app.listen(3333)
+app.post('/', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+});
+
+app.listen(3333);
